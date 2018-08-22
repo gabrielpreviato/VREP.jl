e92d23b6763f674a0665f1543570da861da6fa53
Gabriel Previato <gabriel.previato@gmail.com>
2018-08-22 20:54:57 -0300

    code fixes, added python directory with python-vrep wrapper

--- src/VREPFunc/VREPFunc.jl
+++ src/VREPFunc/VREPFunc.jl
@@ -1,26 +1,26 @@
 function simxGetJointPosition(clientID, jointHandle, operationMode)
-    '''
+    #=
     Please have a look at the function description/documentation in the V-REP user manual
-    '''
+    =#
     position = Ref{Cfloat}
     ret = ccall((:simxGetJointPosition, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, position, operationMode)
 
     return ret, position
 end
 
-function simxSetJointPosition(clientID, jointHandle, position, operationMode):
-    '''
+function simxSetJointPosition(clientID, jointHandle, position, operationMode)
+    #=
     Please have a look at the function description/documentation in the V-REP user manual
-    '''
+    =#
 
     ret = ccall((:simxSetJointPosition, "remoteApi.so"), Cint, (Cint, Cint, Cfloat, Cint), clientID, jointHandle, position, operationMode)
     return ret
 end
 
-function simxGetJointMatrix(clientID, jointHandle, operationMode):
-    '''
+function simxGetJointMatrix(clientID, jointHandle, operationMode)
+    #=
     Please have a look at the function description/documentation in the V-REP user manual
-    '''
+    =#
 
     matrix = Array{Cfloat}(undef, 12)
     ret = ccall((:simxGetJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode))
@@ -28,31 +28,31 @@ function simxGetJointMatrix(clientID, jointHandle, operationMode):
     return ret, matrix
 
 
-function simxSetSphericalJointMatrix(clientID, jointHandle, matrix, operationMode):
-    '''
+function simxSetSphericalJointMatrix(clientID::Cint, jointHandle::Cint, matrix::Array{Cfloat}, operationMode::Cint)
+    #=
     Please have a look at the function description/documentation in the V-REP user manual
-    '''
-    matrix = (ct.c_float*12)(*matrix)
-    if matrix.ndims == 1 && size(matrix, 1) == 12
-        ret = 
-    else
+    =#
 
+    if (matrix.ndims == 1 && size(matrix, 1) == 12):
+        ret = ccall((:simxSetSphericalJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode))
+        return ret
+    else:
+        #throw(MethodError(""))
     end
-    return c_SetSphericalJointMatrix(clientID, jointHandle, matrix, operationMode)
 end
 
-function simxSetJointTargetVelocity(clientID, jointHandle, targetVelocity, operationMode):
-    '''
+function simxSetJointTargetVelocity(clientID, jointHandle, targetVelocity, operationMode)
+    #=
     Please have a look at the function description/documentation in the V-REP user manual
-    '''
+    =#
 
     return c_SetJointTargetVelocity(clientID, jointHandle, targetVelocity, operationMode)
 end
 
-function simxSetJointTargetPosition(clientID, jointHandle, targetPosition, operationMode):
-    '''
+function simxSetJointTargetPosition(clientID, jointHandle, targetPosition, operationMode)
+    #=
     Please have a look at the function description/documentation in the V-REP user manual
-    '''
+    =#
 
     return c_SetJointTargetPosition(clientID, jointHandle, targetPosition, operationMode)
 end
