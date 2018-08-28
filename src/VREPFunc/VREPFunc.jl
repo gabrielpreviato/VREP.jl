250fd956d05d50d88995c58d7f35310d3aae1de3
Gabriel Previato <gabriel.previato@gmail.com>
2018-08-28 13:18:05 -0300

    New functions

--- src/VREPFunc/VREPFunc.jl
+++ src/VREPFunc/VREPFunc.jl
@@ -22,37 +22,109 @@ function simxGetJointMatrix(clientID, jointHandle, operationMode)
     Please have a look at the function description/documentation in the V-REP user manual
     =#
 
-    matrix = Array{Cfloat}(undef, 12)
-    ret = ccall((:simxGetJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode))
+    matrix = Array{Cfloat, 1}(undef, 12)
+    ret = ccall((:simxGetJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode)
 
     return ret, matrix
+end
 
-
-function simxSetSphericalJointMatrix(clientID::Cint, jointHandle::Cint, matrix::Array{Cfloat}, operationMode::Cint)
+function simxSetSphericalJointMatrix(clientID::Cint, jointHandle::Cint, matrix::Array{Cfloat, 1}, operationMode::Cint)
     #=
     Please have a look at the function description/documentation in the V-REP user manual
     =#
 
-    if (matrix.ndims == 1 && size(matrix, 1) == 12):
-        ret = ccall((:simxSetSphericalJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode))
-        return ret
-    else:
+    if (matrix.ndims == 1 && size(matrix, 1) == 12)
+        return ccall((:simxSetSphericalJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode)
+    else
+        println("Passing Array with different size as expected (expected Array with size 12, received " + size(matrix, 1) + "). Matrix's values may be compromised")
+        return ccall((:simxSetSphericalJointMatrix, "remoteApi.so"), Cint, (Cint, Cint, Ref{Cfloat}, Cint), clientID, jointHandle, matrix, operationMode)
         #throw(MethodError(""))
     end
 end
 
-function simxSetJointTargetVelocity(clientID, jointHandle, targetVelocity, operationMode)
+function simxSetJointTargetVelocity(clientID::Cint, jointHandle::Cint, targetVelocity::Cfloat, operationMode::Cint)
     #=
     Please have a look at the function description/documentation in the V-REP user manual
     =#
 
-    return c_SetJointTargetVelocity(clientID, jointHandle, targetVelocity, operationMode)
+    return ccall((:simxSetJointTargetVelocity, "remoteApi.so"), Cint, (Cint, Cfloat, Cint), clientID, jointHandle, targetVelocity, operationMode)
 end
 
-function simxSetJointTargetPosition(clientID, jointHandle, targetPosition, operationMode)
+function simxSetJointTargetPosition(clientID, jointHandle, targetPosition::Cfloat, operationMode)
     #=
     Please have a look at the function description/documentation in the V-REP user manual
     =#
 
-    return c_SetJointTargetPosition(clientID, jointHandle, targetPosition, operationMode)
+    return ccall((:simxSetJointTargetPosition, "remoteApi.so"), Cint, (Cint, Cint, Cfloat, Cint), clientID, jointHandle, targetPosition, operationMode)
+end
+
+function simxJointGetForce(clientID, jointHandle, operationMode)
+    #=
+    Please have a look at the function description/documentation in the V-REP user manual
+    =#
+    println("DEPRECATED. Use simxGetJointForce instead.")
+    force = Cfloat
+    return ccall((:simxGetJointForce, "remoteApi.so"), Cint, (Cint, Cint, Cfloat, Cint), clientID, jointHandle, force, operationMode), force
+end
+
+function simxGetJointForce(clientID, jointHandle, operationMode)
+    #=
+    Please have a look at the function description/documentation in the V-REP user manual
+    =#
+    force = Cfloat
+    return ccall((:simxGetJointForce, "remoteApi.so"), Cint, (Cint, Cint, Cfloat, Cint), clientID, jointHandle, force, operationMode), force
+end
+function simxSetJointForce(clientID, jointHandle, force::Cfloat, operationMode)
+
+    #=
+    Please have a look at the function description/documentation in the V-REP user manual
+    =#
+    return ccall((:simxSetJointForce, "remoteApi.so"), Cint, (Cint, Cint, Cfloat, Cint), clientID, jointHandle, force, operationMode)
+end
+
+function simxReadForceSensor(clientID, forceSensorHandle, operationMode)
+    #=
+    =#
+    Please have a look at the function description/documentation in the V-REP user manual
+    state = Cuchar
+    forceVector = Array{Cfloat, 1}(undef, 3)
+    torqueVector = Array{Cfloat, 1}(undef, 3)
+    ret = ccall((:simxReadForceSensor, "remoteApi.so"), Cint, (Cint, Cint, Cuchar, Ref{Cfloat}, Ref{Cfloat}, Cint), clientID, forceSensorHandle, state, forceVector, torqueVector, operationMode)
+    return ret, state, forceVector, torqueVector
+end
+
+function simxBreakForceSensor(clientID, forceSensorHandle, operationMode)
+    #=
+    Please have a look at the function description/documentation in the V-REP user manual
+    =#
+    return ccall((:simxBreakForceSensor, "remoteApi.so"), Cint, (Cint, Cint, Cint), clientID, forceSensorHandle, operationMode)
+end
+
+function simxReadVisionSensor(clientID, sensorHandle, operationMode)
+    #=
+    Please have a look at the function description/documentation in the V-REP user manual
+    =#
+
+    #detectionState = ct.c_ubyte()
+    detectionState = Cuchar
+    #auxValues      = ct.POINTER(ct.c_float)()
+    auxValues = Ref{Cfloat}
+    #auxValuesCount = ct.POINTER(ct.c_int)()
+    auxValuesCount = Ref{Cint}
+    ret = ccall((:simxReadVisionSensor, "remoteApi.so"), Cint, (Cint, Cint, Cuchar, Ref{Cfloat}, Ref{Cint}, Cint), clientID, sensorHandle, detectionState, auxValues, auxValuesCount, operationMode)
+
+    auxValues2 = []
+    if ret == 0
+        s = 1
+        for i = 1:auxValuesCount[1]
+            auxValues2.append(auxValues[s : s + auxValuesCount[i+1] - 1])
+            s += auxValuesCount[i+1]
+        end
+
+        #free C buffers
+        c_ReleaseBuffer(auxValues)
+        c_ReleaseBuffer(auxValuesCount)
+    end
+
+    return ret, Bool(detectionState != 0), auxValues2
 end
